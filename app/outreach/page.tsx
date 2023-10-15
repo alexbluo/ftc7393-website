4d06c16418e73051c13189739839664ac2ecf4e0
Alex Luo <alexluo92823@gmail.com>
2023-10-15 17:06:49 -0400

    fix typos

--- app/outreach/page.tsx
+++ app/outreach/page.tsx
@@ -7,35 +7,43 @@ const Outreach = () => {
 
       <div className="flex flex-col gap-24 w-full items-center">
         <Bit path="/outreach_scrimmage_oct2023.jpg">
-	We ran an early season scrimmage with 70 people at APL.
-	</Bit>
+          We ran an early season scrimmage with 70 people at APL.
+        </Bit>
         <Bit path="/outreach_dart_watch.jpg">
-	We watched the DART impact together in October 2022.
-	</Bit>
+          We watched the DART impact together in October 2022.
+        </Bit>
         <Bit path="/outreach_sugo1.jpg">
-	We did a fun robotics workshop using SuGO kits in March 2023.
-	</Bit>
+          We did a fun robotics workshop using SuGO kits in March 2023.
+        </Bit>
         <Bit path="/outreach_robot_repair.jpg">
-	At the qualifier we hosted, we also set up a robot repair station.
-	</Bit>
+          At the qualifier we hosted, we also set up a robot repair station.
+        </Bit>
         <Bit path="/outreach_STEM_night.jpg">
-	We brought multiple robots to STEM night at an elementary school.
-	</Bit>
+          We brought multiple robots to STEM night at an elementary school.
+        </Bit>
         <Bit path="/outreach_APC_visit.jpg">
-	We visited and learned about Allied Power and Control, one of our sponsors, and a place where one of our alumni works. they make machined parts for us!
-	</Bit>
+          We visited and learned about Allied Power and Control, one of our
+          sponsors and a place where one of our alumni works. They make machined
+          parts for us!
+        </Bit>
         <Bit path="/outreach_AMT.png">
-	We gave a Shrk-Tank-sylte pitch for sponsorship at the Association of Manufacturing Technologyu in April 2023.
+          We gave a Shark Tank style pitch for sponsorship at the Association of
+          Manufacturing Technologyu in April 2023.
         </Bit>
         <Bit path="/outreach_BAE.png">
-	We met with engineers at BAE systems, and they showed us a 3D virtaul development environment and we told them about our team and gave them a robot demo.
+          We met with engineers at BAE systems, and they showed us a 3D virtual
+          development environment and we told them about our team and gave them
+          a robot demo.
         </Bit>
         <Bit path="/outreach_MTI_volunteer.jpg">
-	We volunteered for the second year in a row at the Maryland Tech Invitational, a very prestigious tournament at the same venue where we hosted our qualifier.
+          We volunteered for the second year in a row at the Maryland Tech
+          Invitational, a very prestigious tournament at the same venue where we
+          hosted our qualifier.
         </Bit>
-	<Bit path="/outreach_sri_at_DC.jpg">
-	One of our alumni volunteering at a Qualifier in DC thourhg the FIRST Chesapeake Alumni Ambassador program.
-	</Bit>
+        <Bit path="/outreach_sri_at_DC.jpg">
+          One of our alumni volunteering at a Qualifier in DC through the FIRST
+          Chesapeake Alumni Ambassador program.
+        </Bit>
       </div>
     </div>
   );
--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -9,37 +9,40 @@ const Sponsors = () => {
 
       <div className="flex flex-col gap-24 w-full items-center">
         <Bit path="/sponsors/PIE3.jpeg">
-	  The Partnership and Inspiration for Engineering Educatoin and Entrepreneurship is the non-profit organization that houses our team.
+          The Partnership and Inspiration for Engineering Education and
+          Entrepreneurship is the non-profit organization that houses our team.
         </Bit>
-	<Bit path="/sponsors/MSBR.jpeg">
-	The Maryland Space Business Roundtable sponosr many STEM activities, and enabled us to give SuGO workshops for free.
+        <Bit path="/sponsors/MSBR.jpeg">
+          The Maryland Space Business Roundtable sponosr many STEM activities,
+          and enabled us to give SuGO workshops for free.
         </Bit>
-	<Bit path="/sponsors/amt.jpeg">
-	The Association of Manufacturing Technology sponsored us for our trip to Worlds.
-	</Bit>
-	<Bit path="/sponsors/allied_power_and_control.png">
-	One of our team alumni works at Allied Power and Control, and they ffer machining services to the team!
-	</Bit>
-	<Bit path="/sponsors/Dassault.png">
-	We use the Solidworks CAD prgram using a license provided by Dassault Systems, and also their systems engineering software.
-	</Bit>
-	<Bit path="/sponsors/FIRST_Chesapeake.png">
-	Our local FIRST organization also helps our team.
-	</Bit>
-	<Bit path="/sponsors/ManTech.png">
-	ManTech sponsored us for our tip to worlds in 2023.
-	</Bit>
-	<Bit path="/sponsors/NASA.png">
-	NASA has loaned us a field border.
-	</Bit>
-	<Bit path="/sponsors/Qualcomm.png">
-	Qualcomm sponsored us for our trip to Worlds in 2023.
-	</Bit>
-	<Bit path="/sponsors/Techaction.png">
-	TechAction helped us pay for an operate our first qualifier in January 2023.
-	</Bit>
-	<Bit path="/sponsors/apl.pdf">
-	</Bit>
+        <Bit path="/sponsors/amt.jpeg">
+          The Association of Manufacturing Technology sponsored us for our trip
+          to Worlds.
+        </Bit>
+        <Bit path="/sponsors/allied_power_and_control.png">
+          One of our team alumni works at Allied Power and Control, and they
+          offer machining services to the team!
+        </Bit>
+        <Bit path="/sponsors/Dassault.png">
+          We use the Solidworks CAD program using a license provided by Dassault
+          Systems, and also their systems engineering software.
+        </Bit>
+        <Bit path="/sponsors/FIRST_Chesapeake.png">
+          Our local FIRST organization also helps our team.
+        </Bit>
+        <Bit path="/sponsors/ManTech.png">
+          ManTech sponsored us for our trip to worlds in 2023.
+        </Bit>
+        <Bit path="/sponsors/NASA.png">NASA has loaned us a field border.</Bit>
+        <Bit path="/sponsors/Qualcomm.png">
+          Qualcomm sponsored us for our trip to Worlds in 2023.
+        </Bit>
+        <Bit path="/sponsors/Techaction.png">
+          TechAction helped us pay for an operate our first qualifier in January
+          2023.
+        </Bit>
+        <Bit path="/sponsors/apl.pdf"></Bit>
       </div>
     </div>
   );
