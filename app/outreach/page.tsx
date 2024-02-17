78273f4e0a86ab740de487a350b87c419c30c431
FalseMSP <55561022+FalseMSP@users.noreply.github.com>
2024-02-17 17:29:06 +0000

    i made it to slide 20 of the outreach notebook.

--- app/outreach/page.tsx
+++ app/outreach/page.tsx
@@ -4,9 +4,45 @@ const Outreach = () => {
   return (
     <div className="">
       <h2 className="text-7xl font-display text-yellow">OUTREACH</h2>
-
       <div className="flex flex-col gap-24 w-full items-center">
-      
+      <Bit path="/outreach_IIT.png">
+      we hosted a stand at the panIIT 2024 global conference, where we reached out to over 1500 panIIT alumni over the course of 10 hours.
+        </Bit>
+      <Bit path="/outreach_sugo3.png">
+      We visited a local scout troop to showcase what you learn as part of the FIRST community,
+      getting the members interested in their possible future in STEM and FIRST! We demonstrated our robot and
+      let them build SUGO robots
+        </Bit>
+      <Bit path="/outreach_tree.png">
+      The Freedom District Lions Club hosted a charity event to raise money for children with cancer.
+      We created ornaments to decorate a tree.
+        </Bit>
+      <Bit path="/outreach_hackathon.png">
+      We hosted a workshop at a hackathon in Tysons, Virginia.
+      High school students from across the DMV area, many of whom were first-time hackers, 
+      got a chance to explore robotics through a hands-on Raspberry Pi project.
+        </Bit>
+      <Bit path="/outreach_msbr.png">
+      Our team enthusiastically presented to the Maryland Space Business Roundtable to seek funding to go towards SuGO workshops,
+      robotic parts, and team registration fees.
+      We were able to secure a $1500 grant. 
+        </Bit>
+      < Bit path="/outreach_teach_sugo2.png">
+      Our team organized a complimentary SuGO workshop at a local library, offering kids the chance to construct their own Lego
+      robots from scratch. Participants engaged in a thrilling head-to-head bracket-style tournament,
+      igniting their enthusiasm for robotics. 
+        </Bit>
+      <Bit path="/outreach_scrimmage_oct2023.jpg">
+          We ran an early season scrimmage with 70 people at APL.
+        </Bit>
+      <Bit path="/outreach_why_turret.jpg">
+          Our team gave an hour-long presentation to Mechanical Paradox alumni about our robot strategy and design, and gained useful insight on hardware concerns and solutions for our upcoming robot.
+      </Bit>
+      <Bit path="/outreach_APC_visit.jpg">
+          We visited and learned about Allied Power and Control, one of our
+          sponsors and a place where one of our alumni works. They make machined
+          parts for us!
+        </Bit>
       < Bit path="/outreach_teach_sugo.png">
       We conducted a workshop on organizing a SuGO workshop outreach initiatives, emphasizing fundraising and community engagement to generate interest in robotics.
         </Bit>
@@ -27,9 +63,6 @@ const Outreach = () => {
         The team showcased its projects and demonstrated the robot at River Hill High School.
         </Bit>
 
-        <Bit path="/outreach_scrimmage_oct2023.jpg">
-          We ran an early season scrimmage with 70 people at APL.
-        </Bit>
         <Bit path="/outreach_dart_watch.jpg">
           We watched the DART impact together in October 2022.
         </Bit>
@@ -41,11 +74,6 @@ const Outreach = () => {
         </Bit>
         <Bit path="/outreach_STEM_night.jpg">
           We brought multiple robots to STEM night at an elementary school.
-        </Bit>
-        <Bit path="/outreach_APC_visit.jpg">
-          We visited and learned about Allied Power and Control, one of our
-          sponsors and a place where one of our alumni works. They make machined
-          parts for us!
         </Bit>
         <Bit path="/outreach_AMT.png">
           We gave a Shark Tank style pitch for sponsorship at the Association of
